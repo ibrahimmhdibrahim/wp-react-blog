81e0d3fa57e44b5ab196e2a5ec5be2ecac1df522
ibrahimnumu <ibrahim.ibrahim@numumarket.com>
2022-05-23 23:09:26 +0300

    fetching posts from wp rest api

--- frontend/src/components/post/post.js
+++ frontend/src/components/post/post.js
@@ -1,11 +1,11 @@
 import classes from "./post.module.css";
 
-const Post = () => {
+const Post = (props) => {
     return (
         <article className={classes.post}>
             <img className={classes.postImage} src="http://via.placeholder.com/400x400" alt="article" />
-            <h3 className={classes.postTitle}>My Fancy Article</h3>
-            <time className={classes.postDate}>22/5/2022</time>
+            <h3 className={classes.postTitle}>{props.title}</h3>
+            <time className={classes.postDate}>{props.date}</time>
         </article>
     );
 }
--- frontend/src/components/posts/Posts.js
+++ frontend/src/components/posts/Posts.js
@@ -1,18 +1,39 @@
 import classes from "./posts.module.css";
 
 import Post from "../post/post";
+import {useEffect, useState} from "react";
 
 const Posts = () => {
+
+    const [postsData, setPostsData] = useState([]);
+
+    useEffect(
+        () => {
+
+            const fetchData = async () => {
+                const fetchPosts = await fetch("http://wp-react-blog.test/wp-json/wp/v2/posts", {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                });
+                const fetchPostsJson = await fetchPosts.json();
+                setPostsData(fetchPostsJson);
+                console.log(fetchPostsJson)
+            }
+            fetchData();
+        }, [setPostsData]
+    );
+
+    const displayPosts = () => {
+        return postsData.map((postData, index) => {
+            return <Post key={index} title={postData.title.rendered} date={postData.date} />;
+        });
+    }
+
     return (
         <div className={classes.posts}>
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
+            {displayPosts()}
         </div>
     );
 }
